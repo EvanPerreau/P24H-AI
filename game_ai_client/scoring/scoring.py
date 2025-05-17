68a069421d1d582746b766466fc011739d6146f2
Evan Perreau <114474918+EvanPerreau@users.noreply.github.com>
2025-05-17 15:21:08 +0200

    add scoring for monster and card

--- game_ai_client/scoring/scoring.py
+++ game_ai_client/scoring/scoring.py
@@ -1,8 +1,10 @@
 from ..models import Monstre
 from ..models import Pioche
+from ..models import Joueur
+from ..models import TypeCarte
 
 class Scoring:
-    def __init__(self, monstres: list[Monstre], cartes: list[Pioche]):
+    def __init__(self, monstres: list[Monstre], cartes: list[Pioche], me: Joueur, fdr: int):
         """
         Initialise le système de scoring pour le jeu.
 
@@ -12,5 +14,106 @@ class Scoring:
         """
         self.monstres = monstres
         self.cartes = cartes
+        self.me = me
+        self.fdr = fdr
+        self.scored_monstres = []
+        self.scored_cartes = []
+        self.set_score_monstres()
+        self.set_score_cartes()
 
+    def get_scored_monstres(self):
+        """
+        Retourne la liste des monstres avec leurs scores.
+
+        Returns:
+            list: Liste de dictionnaires contenant l'index et le score des monstres
+        """
+        return self.scored_monstres
+    
+    def get_scored_cartes(self):
+        """
+        Retourne la liste des cartes avec leurs scores.
+
+        Returns:
+            list: Liste de dictionnaires contenant l'index et le score des cartes
+        """
+        return self.scored_cartes
+
+    def set_score_monstres(self):
+        """
+        Définit le score du monstre.
+
+        Args:
+            monstre: Instance de la classe Monstre
+        """
+        for monstre in self.monstres:
+            vie_max = monstre.gain_savoir / 4
+            multiplicateur_vie_restante = (((monstre.vie - 1) / (vie_max -1)) * 0.5 - 1) * -1
+
+            multiplicateur_monstre_oneshot = 1
+            if self.me.score_attaque >= monstre.vie:
+                multiplicateur_monstre_oneshot = 1.5
+
+            score = monstre.gain_savoir * multiplicateur_vie_restante * multiplicateur_monstre_oneshot
+            
+            self.scored_monstres.append({
+                "index": monstre.index,
+                "score": score
+            })
+
+    def set_score_cartes(self):
+        """
+        Définit le score des cartes.
+
+        Args:
+            carte: Instance de la classe Pioche
+        """
+        for carte in self.cartes:
+
+            score_valeur = 0
+
+            if carte.type_carte == TypeCarte.DEFENSE:
+                multiplicateur_fdr = 1
+                if self.fdr > self.me.score_defense:
+                    defense = self.fdr - self.me.score_defense
+                    multiplicateur_fdr = 1 + defense / 10
+
+                multiplicateur_hard_danger = 1
+                if self.fdr > self.me.score_defense + self.me.vie:
+                    multiplicateur_hard_danger = 100
+
+                score_valeur = carte.valeur * multiplicateur_fdr * multiplicateur_hard_danger
+
+            elif carte.type_carte == TypeCarte.ATTAQUE:
+                multiplicateur_no_attaque = 1
+                if self.me.score_attaque == 0:
+                    multiplicateur_no_attaque = 5
+                
+                multiplicateur_no_monstre = 1
+                if self.scored_monstres.__len__() == 0:
+                    multiplicateur_no_monstre = 0
+
+                score_monstre_max = 0
+                for monstre in self.scored_monstres:
+                    if monstre["score"] > score_monstre_max:
+                        score_monstre_max = monstre["score"]/10
+                    
+                if score_monstre_max == 0:
+                    score_monstre_max = 1
+
+                score_valeur = carte.valeur * multiplicateur_no_attaque * multiplicateur_no_monstre * score_monstre_max
+                
+            elif carte.type_carte == TypeCarte.SAVOIR:
+                multiplicateur_no_monstre = 1
+                if self.scored_monstres.__len__() == 0:
+                    multiplicateur_no_monstre = 5
+                else:
+                    multiplicateur_no_monstre = 1/self.scored_monstres.__len__()
+
+                score_valeur = carte.valeur * multiplicateur_no_monstre
+
+            self.scored_cartes.append({
+                "index": carte.index,
+                "score": score_valeur
+            })
     
